baef7bf956d2f2405463a0d2010f435ffb20f64a
Xianghai <sxh.exe@gmail.com>
2018-10-27 21:28:20 -0700

    test claison rearrangement with increasing number of active atoms

--- timetest.py
+++ timetest.py
@@ -6,13 +6,15 @@ reactants = ['CCO', 'CCCO', 'CCCCO', 'CCCCCO']
 products = ['C=C.O', 'CC=C.O', 'CCC=C.O', 'CCCC=C.O']
 reactant = 'C=CCOC(=CC)C'
 product = 'CC(C(=O)C)CC=C'
+activeList = '1,2,3,4,5,6'
 
 with open('timetest_{}.log'.format(reactant), 'w') as logfile:
     for i in range(1):
-        job = rr.ReactionRoute(inputJson='{{"reactant": "{}", "product": "{}"}}'.format(reactant, product))
+        job = rr.ReactionRoute(inputJson='{{"reactant": "{}", "product": "{}", "activeList": [{}]}}'.format(reactant, product, activeList))
         startTime = datetime.now()
         head, target = job.isomerSearch()
         endTime = datetime.now()
-    	logfile.write('time used: {}\nnumber of nodes: {}'.format((endTime - startTime).total_seconds(), len(job._reactionMap)))
-        reactant = 'C' + reactant
-        product = 'C' + product
+        logfile.write('time used: {}, number of nodes: {}, activeList: {}\n'.format((endTime - startTime).total_seconds(), len(job._reactionMap), activeList))
+        # reactant = 'C' + reactant
+        # product = 'C' + product
+        activeList += ',{},{}'.format(7+2*i, 7+2*i+1)
