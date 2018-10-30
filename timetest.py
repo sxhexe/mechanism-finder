29db731e90cbb14a188019069eb9b35b04acc849
Xianghai Sheng <xsheng@ucmerced.edu>
2018-10-29 17:31:08 -0700

    add source and target size in timetest.py

--- timetest.py
+++ timetest.py
@@ -7,18 +7,21 @@ products = ['C=C.O', 'CC=C.O', 'CCC=C.O', 'CCCC=C.O']
 # reactant = 'C=CCOC(=CC)C'
 # product = 'CC(C(=O)C)CC=C'
 # activeList = '1,2,3,4,5,6'
-reactant = 'CCO'
-product = 'C=C.O'
+reactant = 'CC=O'
+product = 'C=CO'
 
 with open('timetest_{}.log'.format(reactant), 'w') as logfile:
     for i in range(5):
         # job = rr.ReactionRoute(inputJson='{{"reactant": "{}", "product": "{}", "activeList": [{}]}}'.format(reactant, product, activeList))
-        job = rr.ReactionRoute(inputJson='{{"reactant": "{}", "product": "{}"}'.format(reactant, product))
+        job = rr.ReactionRoute(inputJson='{{"reactant": "{}", "product": "{}"}}'.format(reactant, product))
         startTime = datetime.now()
         head, target = job.isomerSearch()
         endTime = datetime.now()
         # logfile.write('time used: {}, number of nodes: {}, activeList: {}\n'.format((endTime - startTime).total_seconds(), len(job._reactionMap), activeList))
-        logfile.write('time used: {}, number of nodes: {}, srcSize: \n'.format((endTime - startTime).total_seconds(), len(job._reactionMap), job.eSourceSizeRunningSum[0]))
+	sourceTime = job.eSourceSizeRunningSum
+	targetTime = job.eTargetSizeRunningSum
+	logfile.write('source size running average: {}, target size running average: {}'.format(sourceTime[0]/sourceTime[1], targetTime[0]/targetTime[1]))
+        logfile.write('time used: {}, number of nodes: {}\n'.format((endTime - startTime).total_seconds(), len(job._reactionMap)))
         reactant = 'C' + reactant
         product = 'C' + product
         # activeList += ',{},{}'.format(7+2*i, 7+2*i+1)
